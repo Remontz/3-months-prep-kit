b5e84f56ee4a77d5c3d492601c1c3083641f5331
Kacy Gilbert <gilbertkacy90@gmail.com>
2023-01-24 23:10:26 -0600

    Completed CountingSort1

--- Week 2/CountingSort1.js
+++ Week 2/CountingSort1.js
@@ -24,16 +24,16 @@
 // Given a list of integers, count and return the number of times each value appears as an array of integers.
 
 const countingSort = (arr) => {
-    const countingArr = []
-    for (let i = 0; i < 100; i++) {
-        countingArr.push(i)
+    let countingArr = new Array(100)
+
+    for (let x = 0; x < countingArr.length; x++) {
+        countingArr[x] = 0
     }
 
     for (let i = 0; i < arr.length; i++) {
         for (let t = 0; t < 100; t++) {
             if (t === arr[i]) {
                 countingArr[t] = countingArr[t] + 1
-                continue
             }
 
         }
@@ -41,3 +41,5 @@ const countingSort = (arr) => {
 
     return countingArr
 }
+const testArray = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 9, 29, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33]
+console.log(countingSort(testArray))
